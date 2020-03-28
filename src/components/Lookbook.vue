18ea98eaa8ccc4fedabf1b529ce1286adf3ca94d
HelloSummerCc <694858043@qq.com>
2020-03-28 20:45:52 +0800

    fist commit

---
+++ src/components/Lookbook.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <Banner />
+    <div class="img-box">
+      <img src="http://ilovechoc.hk/look.png">
+      <img src="http://ilovechoc.hk/back.jpg" class="top-img" @click="topClick">
+    </div>
+    <BottomBanner />
+  </div>
+</template>
+
+<script>
+import Banner from './banner';
+import BottomBanner from './BottomBanner';
+const J = window.jQuery;
+export default {
+  name: 'Lookbook',
+  components: {
+    Banner,
+    BottomBanner
+  },
+  data () {
+    return {}
+  },
+  beforeMount() {},
+  mounted() {
+    this.$router.afterEach((to, from, next) => {
+      window.scrollTo(0, 0)
+    })
+  },
+  methods: {
+    topClick() {
+      J('html , body').animate({scrollTop: 0},'slow');
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.img-box {
+  position: relative;
+}
+img {
+  display: block;
+  width: 100%; 
+}
+.top-img {
+  width: 60px;
+  position: absolute;
+  right: 20px;
+  bottom: 20px;
+}
+</style>
+
+
+
